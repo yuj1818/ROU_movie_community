ff5cd49fab87bc06eb3688c3ef94ee1c543aabd9
sonyujeong <sonyujeong18@amsquare.ai>
2025-02-14 16:31:31 +0900

    init: react 초기 설정

---
+++ front/src/App.jsx
@@ -0,0 +1,15 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />,
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
