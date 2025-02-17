27b0bf3ac1557047f38ac17c292b4920973db416
sonyujeong <sonyujeong18@amsquare.ai>
2025-02-17 11:25:01 +0900

    feat: 메인 페이지 트렌드 영화 목록 캐러셀 구현

--- front/src/components/home/MovieCarousel.jsx
+++ front/src/components/home/MovieCarousel.jsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import 'keen-slider/keen-slider.min.css';
 import { useKeenSlider } from 'keen-slider/react';
 import { getTrendMovieList } from '../../utils/movieApi';
+import Url from '../../constants/URL';
 
 const Container = styled.div`
   height: 30rem;
@@ -12,17 +13,50 @@ const Container = styled.div`
 `;
 
 const Slide = styled.div`
-  background-color: white;
   display: flex;
   align-items: center;
   justify-content: center;
   border-radius: 0.5rem;
+  position: relative;
+
+  .media {
+    width: 100%;
+    height: 100%;
+    z-index: 1;
+    object-fit: cover;
+    border-radius: 0.5rem;
+  }
+`;
+
+const Description = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  color: white;
+  background-image: linear-gradient(to top right, black, rgba(0, 0, 0, 0) 60%);
+  z-index: 2;
+  padding: 2rem 40% 2rem 3rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  justify-content: flex-end;
+
+  .title {
+    font-size: 2rem;
+    font-family: 'Pretendard-SemiBold';
+  }
+
+  .overview {
+    font-size: 1rem;
+    line-height: 150%;
+  }
 `;
 
 const MovieCarousel = () => {
   const intervalRef = useRef(null);
   const [isPlaying, setIsPlaying] = useState(true);
-  const [trendData, setTrendData] = useState([]);
+  const [trendData, setTrendData] = useState();
+  const [hoveredIdx, setHoveredIdx] = useState(null);
 
   const [sliderRef, slider] = useKeenSlider({
     loop: true,
@@ -39,7 +73,7 @@ const MovieCarousel = () => {
 
     intervalRef.current = setInterval(() => {
       slider.current?.next();
-    }, 2000);
+    }, 3000);
 
     return () => {
       if (intervalRef.current) {
@@ -50,7 +84,7 @@ const MovieCarousel = () => {
 
   const getTrendData = async () => {
     const res = await getTrendMovieList();
-    console.log(res);
+    setTrendData(res);
   };
 
   useEffect(() => {
@@ -58,17 +92,42 @@ const MovieCarousel = () => {
   }, []);
 
   return (
-    <Container
-      ref={sliderRef}
-      className="keen-slider"
-      onMouseEnter={() => setIsPlaying(false)}
-      onMouseLeave={() => setIsPlaying(true)}
-    >
-      <Slide className="keen-slider__slide">1</Slide>
-      <Slide className="keen-slider__slide">2</Slide>
-      <Slide className="keen-slider__slide">3</Slide>
-      <Slide className="keen-slider__slide">4</Slide>
-    </Container>
+    trendData && (
+      <Container ref={sliderRef} className="keen-slider">
+        {trendData.map((data, idx) => (
+          <Slide
+            key={data.movie_id}
+            className="keen-slider__slide"
+            onMouseEnter={() => {
+              setHoveredIdx(idx);
+              setIsPlaying(false);
+            }}
+            onMouseLeave={() => {
+              setHoveredIdx(null);
+              setIsPlaying(true);
+            }}
+          >
+            {hoveredIdx === idx ? (
+              <iframe
+                className="media"
+                src={`${Url.youtubePath}${data.videos}?autoplay=1&mute=0&loop=1&controls=0`}
+                allow="autoplay; encrypted-media"
+              />
+            ) : (
+              <img
+                className="media"
+                src={Url.tmdbImgPath + data.backdrop_path}
+                alt={data.title}
+              />
+            )}
+            <Description>
+              <p className="title">{data.title}</p>
+              <p className="overview">{data.overview}</p>
+            </Description>
+          </Slide>
+        ))}
+      </Container>
+    )
   );
 };
 
---
+++ front/src/constants/Url.js
@@ -0,0 +1,6 @@
+const Url = {
+  tmdbImgPath: 'https://image.tmdb.org/t/p/original/',
+  youtubePath: 'https://www.youtube.com/embed/',
+};
+
+export default Url;
