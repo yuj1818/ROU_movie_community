3496e0773126f948b2be603593305d7eefb03eb6
sonyujeong <sonyujeong18@amsquare.ai>
2025-02-18 13:58:59 +0900

    style: 스크롤 커스텀 스타일링

--- front/src/components/movie/CastList.jsx
+++ front/src/components/movie/CastList.jsx
@@ -1,14 +1,44 @@
 import { useSelector } from 'react-redux';
 import CastInfo from './CastInfo';
+import styled from 'styled-components';
+import Colors from '../../constants/Colors';
+
+const Container = styled.div`
+  width: 100%;
+  background-color: white;
+  display: flex;
+  flex-wrap: wrap;
+  max-height: 13rem;
+  overflow-y: auto;
+  border-radius: 0.25rem;
+  color: black;
+  padding: 1rem;
+
+  &::-webkit-scrollbar {
+    width: 15px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: ${Colors.btnDarkPurple};
+    border-radius: 15px;
+    border: 3px solid transparent;
+    background-clip: padding-box;
+  }
+
+  &::-webkit-scrollbar-track {
+    background-color: transparent;
+    border-radius: 15px;
+  }
+`;
 
 const CastList = () => {
   const { movieInfo } = useSelector((state) => state.movie);
 
   return (
-    <div className="w-full bg-white flex flex-wrap max-h-[13rem] overflow-y-auto rounded-sm text-black p-4">
+    <Container>
       {movieInfo.actors &&
         movieInfo.actors.map((actor) => <CastInfo actorInfo={actor} />)}
-    </div>
+    </Container>
   );
 };
 
