97e591a2b96610fce37daf2cf8a82dd8e0443198
sonyujeong <sonyujeong18@amsquare.ai>
2025-02-18 18:16:18 +0900

    style: 로그인 페이지 라우팅 및  로그인 폼 컴포넌트 구현

---
+++ front/src/pages/auth/LoginPage.jsx
@@ -0,0 +1,11 @@
+import LoginForm from '../../components/auth/LoginForm';
+
+const LoginPage = () => {
+  return (
+    <div className="grow flex w-full justify-center items-center">
+      <LoginForm />
+    </div>
+  );
+};
+
+export default LoginPage;
