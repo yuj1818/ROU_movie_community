b7ef97b22a99e9e49111c5083366f911c28462a8
sonyujeong <sonyujeong18@amsquare.ai>
2025-02-19 13:31:39 +0900

    feat: 헤더에 accessToken 자동 부여

--- front/src/utils/axios.js
+++ front/src/utils/axios.js
@@ -1,13 +1,24 @@
 import axios from 'axios';
+import { getCookie } from './cookie';
 
 const URL = import.meta.env.VITE_API_URL + '/api/';
 
 const API = axios.create({
   baseURL: URL,
   withCredentials: true,
-  headers: {
-    'Content-Type': 'application/json',
-  },
 });
+
+API.interceptors.request.use(
+  (config) => {
+    const token = getCookie('token');
+    if (token) {
+      config.headers['Authorization'] = token;
+    }
+    return config;
+  },
+  (err) => {
+    return Promise.reject(err);
+  },
+);
 
 export default API;
