d8c11c38f194cdc1f92f0ee2a6f0ab56513ef03a
sonyujeong <sonyujeong18@amsquare.ai>
2025-02-19 13:17:43 +0900

    feat: redux persist 설정

--- front/src/stores/store.js
+++ front/src/stores/store.js
@@ -1,10 +1,24 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import sessionStorage from 'redux-persist/lib/storage/session';
 import movieSlice from './movie';
+import authSlice from './auth';
+
+const persistConfig = {
+  key: 'root',
+  storage: sessionStorage,
+  whitelist: ['auth'],
+};
 
 const rootReducer = combineReducers({
   movie: movieSlice,
+  auth: authSlice,
 });
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
 });
+
+export const persistor = persistStore(store);
