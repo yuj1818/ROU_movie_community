fe0461fe1d4e21bd856949ee03fd0dd14f332ce6
sonyujeong <sonyujeong18@amsquare.ai>
2025-02-19 11:23:33 +0900

    feat: 로그인 기능 구현

--- front/src/components/auth/LoginForm.jsx
+++ front/src/components/auth/LoginForm.jsx
@@ -1,7 +1,10 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Colors from '../../constants/Colors';
 import { Button } from '../common/Button';
 import { SubTitle } from './SubTitle';
 import tw from 'tailwind-styled-components';
+import { signIn } from '../../utils/authApi';
 
 const Container = tw.div`
   flex flex-col w-full gap-1
@@ -12,16 +15,36 @@ const Label = tw.label`
 `;
 
 const LoginForm = () => {
+  const navigate = useNavigate();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [errMsg, setErrMsg] = useState('');
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const res = await signIn({
+      username,
+      password,
+    });
+    if (res.status === 200) {
+      navigate('/');
+    } else {
+      setErrMsg('아이디와 비밀번호를 다시 확인해주세요');
+    }
+  };
+
   return (
     <div className="w-1/2 py-6 flex flex-col gap-2 bg-neutral-300 rounded-lg items-center max-w-[25rem]">
       <SubTitle>로그인</SubTitle>
-      <form className="w-2/3 flex flex-col gap-2">
+      <form className="w-2/3 flex flex-col gap-2" onSubmit={onSubmit}>
         <Container>
           <Label htmlFor="username">아이디</Label>
           <input
             className="h-[2rem] p-2 w-full rounded-sm"
             type="text"
             id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
           />
         </Container>
         <Container>
@@ -30,9 +53,17 @@ const LoginForm = () => {
             className="h-[2rem] p-2 w-full rounded-sm"
             type="password"
             id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
         </Container>
-        <Button $isFullWidth={true} $background={Colors.btnPurple}>
+        {errMsg && <p className="text-red-600 text-xs text-center">{errMsg}</p>}
+        <Button
+          $marginTop={1}
+          $isFullWidth={true}
+          $background={Colors.btnPurple}
+          disabled={username === '' || password === ''}
+        >
           로그인
         </Button>
       </form>
