daf79ff2923ed27d30bac5fbbd58d696d512057c
sonyujeong <sonyujeong18@amsquare.ai>
2025-02-20 15:07:00 +0900

    feat: 영화 싫어요, 시청, 찜 기능 구현

--- front/src/stores/movie.js
+++ front/src/stores/movie.js
@@ -15,8 +15,25 @@ const movieSlice = createSlice({
       state.movieInfo.isLike = !state.movieInfo.isLike;
       state.movieInfo.like_movie_users_count = action.payload;
     },
+    toggleDislikeMovie(state, action) {
+      state.movieInfo.isDislike = !state.movieInfo.isDislike;
+      state.movieInfo.dislike_movie_users_count = action.payload;
+    },
+    toggleFavoriteMovie(state, action) {
+      state.movieInfo.isFavorite = !state.movieInfo.isFavorite;
+      state.movieInfo.favorite_movie_users_count = action.payload;
+    },
+    toggleWatchMovie(state) {
+      state.movieInfo.isWatch = !state.movieInfo.isWatch;
+    },
   },
 });
 
-export const { setMovieInfo, toggleLikeMovie } = movieSlice.actions;
+export const {
+  setMovieInfo,
+  toggleLikeMovie,
+  toggleDislikeMovie,
+  toggleFavoriteMovie,
+  toggleWatchMovie,
+} = movieSlice.actions;
 export default movieSlice.reducer;
