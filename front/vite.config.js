b86bb57175ab120bcf5d13c4fb722aa73ccc58cf
sonyujeong <sonyujeong18@amsquare.ai>
2025-02-24 17:49:15 +0900

    refactor: 이미지 최적화 성능 개선(26 -> 50)

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,8 +1,44 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
+import viteCompression from 'vite-plugin-compression';
+import viteImagemin from '@vheemstra/vite-plugin-imagemin';
+import imageminWebp from 'imagemin-webp';
+import imageminSvgo from 'imagemin-svgo';
+import imageminPngquant from 'imagemin-pngquant';
+import imageminMozjpeg from 'imagemin-mozjpeg';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'src/assets/*',
+          dest: 'assets',
+        },
+      ],
+    }),
+    viteImagemin({
+      plugins: {
+        jpg: imageminMozjpeg(),
+        svg: imageminSvgo(),
+        png: imageminPngquant(),
+      },
+      makeWebp: {
+        plugins: {
+          jpg: imageminWebp(),
+          png: imageminWebp(),
+          svg: imageminWebp(),
+        },
+      },
+    }),
+    react(),
+    svgr(),
+    viteCompression({
+      algorithm: 'gzip',
+      ext: '.gz',
+    }),
+  ],
 });
