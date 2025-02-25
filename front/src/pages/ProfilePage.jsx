787bb65f7a9a48b023ae10518dfab9a82af73eac
sonyujeong <sonyujeong18@amsquare.ai>
2025-02-25 10:59:35 +0900

    feat: 프로필 페이지 라우팅 설정

--- front/src/pages/ProfilePage.jsx
+++ front/src/pages/ProfilePage.jsx
@@ -1,4 +1,22 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { getProfileData } from '../utils/profileApi';
+import { setProfileInfo } from '../stores/profile';
+
 const ProfilePage = () => {
+  const dispatch = useDispatch();
+  const params = useParams();
+
+  useEffect(() => {
+    const getProfileInfo = async () => {
+      const res = await getProfileData(params.user_id);
+      dispatch(setProfileInfo(res));
+    };
+
+    getProfileInfo();
+  }, [params.user_id]);
+
   return <div></div>;
 };
 
