db2a03a610873dbd8af401aa2c3a2f6e6ae57aaa
sonyujeong <sonyujeong18@amsquare.ai>
2025-02-25 13:49:48 +0900

    feat: 팔로잉 api 호출 함수 구현

--- front/src/utils/profileApi.js
+++ front/src/utils/profileApi.js
@@ -7,3 +7,9 @@ export const getProfileData = (user_id) => {
     .then((res) => res.data)
     .catch((err) => err);
 };
+
+export const follow = (user_id) => {
+  return API.post(URL + `follow/${user_id}/`)
+    .then((res) => res.data)
+    .catch((err) => err);
+};
