5a84b2d095306c529721c499261061257903f5ea
sonyujeong <sonyujeong18@amsquare.ai>
2025-02-26 16:04:27 +0900

    feat: 회원 탈퇴 기능 구현

--- front/src/components/common/Modal.jsx
+++ front/src/components/common/Modal.jsx
@@ -4,10 +4,12 @@ import { useLocation } from 'react-router-dom';
 import { closeModal } from '../../stores/modal';
 import TrailerModal from '../movie/TrailerModal';
 import ProfileEditModal from '../profile/ProfileEditModal';
+import ConfirmModal from '../profile/ConfirmModal';
 
 const MODAL_TYPES = {
   trailer: 'trailer',
   profile: 'profile',
+  confirm: 'confirm',
 };
 
 const MODAL_COMPONENTS = [
@@ -18,6 +20,10 @@ const MODAL_COMPONENTS = [
   {
     type: MODAL_TYPES.profile,
     component: <ProfileEditModal />,
+  },
+  {
+    type: MODAL_TYPES.confirm,
+    component: <ConfirmModal />,
   },
 ];
 
---
+++ front/src/components/profile/ConfirmModal.jsx
@@ -0,0 +1,48 @@
+import { Button } from '../common/Button';
+import Colors from '../../constants/Colors';
+import { useDispatch } from 'react-redux';
+import { closeModal } from '../../stores/modal';
+import { resign } from '../../utils/authApi';
+import { setLogout } from '../../stores/auth';
+import { useNavigate } from 'react-router-dom';
+
+const ConfirmModal = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const onCancel = () => {
+    dispatch(closeModal());
+  };
+
+  const onConfirm = async () => {
+    const res = await resign();
+    if (res.status === 204) {
+      navigate('/');
+      dispatch(setLogout());
+    } else {
+      window.alert('탈퇴 실패');
+    }
+    dispatch(closeModal());
+  };
+
+  return (
+    <div className="bg-white h-fit w-1/3 max-w-[20rem] p-8 rounded-sm flex flex-col justify-center items-center gap-6 relative z-1">
+      <p className="text-sm text-black text-center">정말 탈퇴하시겠습니까?</p>
+      <div className="flex gap-2">
+        <Button
+          $background="white"
+          $boxShadow={`0 0 0 1px ${Colors.btnGray} inset`}
+          $color={Colors.btnGray}
+          onClick={onCancel}
+        >
+          취소
+        </Button>
+        <Button $background="red" onClick={onConfirm}>
+          확인
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default ConfirmModal;
