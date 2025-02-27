b354608596459a0d9f47537b5ed96aaa1081eea8
sonyujeong <sonyujeong18@amsquare.ai>
2025-02-27 16:02:59 +0900

    feat: 좋아요/리뷰/찜/시청 한 영화 목록 조회 api 구현(페이지네이션)

--- back/ACCOUNTS/urls.py
+++ back/ACCOUNTS/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-# testuser1
+# testuser
 # testtest123!
 
 urlpatterns = [
@@ -15,4 +15,6 @@ urlpatterns = [
     path('preference/<str:pType>/', views.preference),
     # 팔로잉(POST)
     path('follow/<int:user_pk>/', views.follow),
+    # 회원 좋아요/리뷰/찜/시청 한 리스트 조회(GET)
+    path('movie/<int:user_pk>/', views.movie_list),
 ]
