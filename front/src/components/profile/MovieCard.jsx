d7e12044ab1f1ede865cbdea61c17ed16719bcf8
sonyujeong <sonyujeong18@amsquare.ai>
2025-02-27 17:55:18 +0900

    feat: 좋아요/리뷰/찜/시청한 영화 목록 조회 기능 구현

---
+++ front/src/components/profile/MovieCard.jsx
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+import Url from '../../constants/URL';
+import { useNavigate } from 'react-router-dom';
+import LazyImg from '../common/LazyImg';
+
+const Container = styled.div`
+  width: 24.5%;
+  aspect-ratio: 3 / 4;
+  cursor: pointer;
+  margin-bottom: 0.5%;
+`;
+
+const MovieCard = ({ data }) => {
+  const navigate = useNavigate();
+
+  return (
+    <Container onClick={() => navigate(`/movie/${data.movie_id}`)}>
+      <LazyImg
+        className="w-full h-full"
+        src={Url.tmdbImgPath + data.poster_path}
+        alt="poster"
+      />
+    </Container>
+  );
+};
+
+export default MovieCard;
--- front/src/components/profile/MovieList.jsx
+++ front/src/components/profile/MovieList.jsx
@@ -1,7 +1,11 @@
 import { useDispatch, useSelector } from 'react-redux';
 import tw from 'tailwind-styled-components';
-import { setTarget } from '../../stores/profile';
-import { useEffect } from 'react';
+import { setPage, setTarget, setTotalItems } from '../../stores/profile';
+import { useEffect, useState } from 'react';
+import { getProfileMovieData } from '../../utils/profileApi';
+import MovieCard from './MovieCard';
+import Pagination from '../common/Pagination';
+import Colors from '../../constants/Colors';
 
 const Category = tw.div`
   w-[25%] px-4 py-2 rounded-t font-pretendard_semibold text-center cursor-pointer
@@ -9,10 +13,43 @@ const Category = tw.div`
 `;
 
 const MovieList = () => {
-  const { targets, targetIdx } = useSelector((state) => state.profile);
+  const { targets, targetIdx, userId, page, totalItems } = useSelector(
+    (state) => state.profile,
+  );
   const dispatch = useDispatch();
+  const [selectedData, setSelectedData] = useState([]);
 
-  useEffect(() => {}, [targetIdx]);
+  const onPageChange = (newPage) => {
+    dispatch(setPage(newPage));
+  };
+
+  useEffect(() => {
+    const getSelectedData = async () => {
+      let target = '';
+      if (targetIdx === 0) {
+        target = 'like';
+      } else if (targetIdx === 1) {
+        target = 'review';
+      } else if (targetIdx === 2) {
+        target = 'favorite';
+      } else {
+        target = 'watch';
+      }
+
+      const res = await getProfileMovieData(userId, {
+        target: target,
+        page: page,
+        limit: 12,
+      });
+
+      setSelectedData(res.results);
+      dispatch(setTotalItems(res.count));
+    };
+
+    if (userId) {
+      getSelectedData();
+    }
+  }, [targetIdx, page, userId]);
 
   return (
     <div className="w-[68%] flex flex-col">
@@ -27,7 +64,21 @@ const MovieList = () => {
           </Category>
         ))}
       </div>
-      <div className="bg-white w-full aspect-4/3 rounded-b rounded-tr p-2"></div>
+      <div className="bg-white w-full aspect-square rounded-b p-2 mb-2">
+        {selectedData && (
+          <div className="flex flex-wrap justify-start gap-[0.5%] w-full">
+            {selectedData.map((data) => (
+              <MovieCard key={data.movie_id} data={data} />
+            ))}
+          </div>
+        )}
+      </div>
+      <Pagination
+        currentPage={page}
+        totalPages={Math.ceil(totalItems / 12)}
+        onPageChange={onPageChange}
+        color={Colors.btnPurple}
+      />
     </div>
   );
 };
