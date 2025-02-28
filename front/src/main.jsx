6a1403e2e248b504ad6354b01ceae3cd93e7ca42
sonyujeong <sonyujeong18@amsquare.ai>
2025-02-28 17:36:06 +0900

    feat: dayjs 설치 및 로컬 설정

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,5 +1,15 @@
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './main.css';
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import isLeapYear from 'dayjs/plugin/isLeapYear';
+import 'dayjs/locale/ko';
+
+dayjs.extend(isLeapYear);
+dayjs.locale('ko');
+dayjs.extend(relativeTime);
 
 createRoot(document.getElementById('root')).render(<App />);
