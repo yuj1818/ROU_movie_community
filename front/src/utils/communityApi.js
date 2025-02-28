8ee89e74e5bea744313b5af060acbc263cf23b0b
sonyujeong <sonyujeong18@amsquare.ai>
2025-02-28 15:49:30 +0900

    feat: 영화 리뷰 상세 정보 조회 api 호출 함수 구현

---
+++ front/src/utils/communityApi.js
@@ -0,0 +1,9 @@
+import API from './axios';
+
+const URL = 'community/';
+
+export const getPostDetail = (review_id) => {
+  return API.get(URL + `${review_id}/`)
+    .then((res) => res.data)
+    .catch((err) => console.error(err));
+};
