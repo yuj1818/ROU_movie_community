f8a971ec2ff152c31c2091e4feca42ddb30c687b
sonyujeong <sonyujeong18@amsquare.ai>
2025-03-05 15:52:53 +0900

    feat: 게시글 목록 조회 기능 구현(정렬 추가)

---
+++ front/src/components/common/post/PostInfo.jsx
@@ -0,0 +1,77 @@
+import tw from 'tailwind-styled-components';
+import { MessageSquareMore, ThumbsUp, ThumbsDown } from 'lucide-react';
+import LazyImg from '../LazyImg';
+import unknown from '../../../assets/profile.png';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+
+const IconContainer = tw.div`
+  flex h-[1rem] items-center text-xs text-black
+`;
+
+const Content = styled.div`
+  font-size: 0.875rem;
+  line-height: 1.25rem;
+  font-family: 'Pretendard-ExtraLight';
+  overflow: hidden;
+  text-overflow: ellipsis;
+  max-width: 100%;
+  display: -webkit-box;
+  white-space: pre-line;
+  -webkit-line-clamp: 1;
+  -webkit-box-orient: vertical;
+`;
+
+const PostInfo = ({ data }) => {
+  const navigate = useNavigate();
+
+  return (
+    <div
+      className="flex items-center gap-4 w-full h-[5rem] px-6 py-4 bg-white rounded-md"
+      onClick={() => navigate(`/review/${data.id}`)}
+    >
+      <LazyImg
+        className="w-[3rem] h-[3rem] rounded-full"
+        src={
+          data.review_writor.profile_image
+            ? data.review_writor.profile_image
+            : unknown
+        }
+        alt="user_img"
+      />
+      <div className="grow flex flex-col gap-2 text-black min-w-0">
+        <p className="text-base overflow-hidden text-ellipsis max-w-full">
+          {data.title}
+        </p>
+        <Content>{data.content}</Content>
+      </div>
+      <div className="flex flex-col justify-between h-full">
+        <div className="flex gap-2">
+          <IconContainer>
+            <MessageSquareMore className="h-full" />
+            <p>{data.comment_count}</p>
+          </IconContainer>
+          <IconContainer>
+            <ThumbsUp className="h-full" />
+            <p>{data.like_count}</p>
+          </IconContainer>
+          <IconContainer>
+            <ThumbsDown className="h-full" />
+            <p>{data.dislike_count}</p>
+          </IconContainer>
+        </div>
+        <p
+          className="underline underline-offset-2 cursor-pointer text-xs font-pretendard_exlight text-gray-400 whitespace-nowrap text-right"
+          onClick={(e) => {
+            e.stopPropagation();
+            navigate(`/profile/${data.review_writor.id}`);
+          }}
+        >
+          by. {data.review_writor.username}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default PostInfo;
--- front/src/components/community/PostList.jsx
+++ front/src/components/community/PostList.jsx
@@ -1,5 +1,45 @@
+import { useEffect, useState } from 'react';
+import { getPostData } from '../../utils/communityApi';
+import { useDispatch, useSelector } from 'react-redux';
+import { setSort, setTotalPages } from '../../stores/community';
+import PostInfo from '../common/post/PostInfo';
+
 const PostList = () => {
-  return <div className="flex flex-col w-full gap-4"></div>;
+  const { page, sort, totalPages } = useSelector((state) => state.community);
+  const dispatch = useDispatch();
+  const [postList, setPostList] = useState([]);
+
+  const getPostList = async () => {
+    const res = await getPostData({
+      page: page,
+      sort: sort,
+    });
+    dispatch(setTotalPages(res.total_pages));
+    setPostList(res.results);
+  };
+
+  useEffect(() => {
+    console.log(page);
+    getPostList();
+  }, [page, sort]);
+
+  return (
+    <div className="flex flex-col w-full gap-4">
+      <select
+        className="text-black w-fit self-end text-sm h-[1.5rem] rounded-sm"
+        name="sort"
+        id="sort-select"
+        value={sort}
+        onChange={(e) => dispatch(setSort(parseInt(e.target.value)))}
+      >
+        <option value={0}>최신순</option>
+        <option value={1}>좋아요 ↓</option>
+        <option value={2}>댓글 ↓</option>
+      </select>
+      {postList &&
+        postList.map((post) => <PostInfo key={post.id} data={post} />)}
+    </div>
+  );
 };
 
 export default PostList;
