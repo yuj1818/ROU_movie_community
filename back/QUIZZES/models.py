2f8b97544c67d570a157c4f75e3598e771ee6a75
sonyujeong <sonyujeong18@amsquare.ai>
2025-03-12 18:06:48 +0900

    feat: QUIZ app 생성 및 모델 생성

---
+++ back/QUIZZES/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.conf import settings
+
+class Quiz(models.Model):
+  question = models.CharField(max_length=100)
+  quiz_writor = models.ForeignKey(
+    settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='written_quiz'
+  )
+  quiz_image = models.ImageField(
+    upload_to='quiz_images/', blank=True, null=True
+  )
+
+class QuizItem(models.Model):
+  quiz = models.ForeignKey(
+    Quiz, related_name='items', on_delete=models.CASCADE
+  )
+  choice_text = models.CharField(max_length=200)
+  is_correct = models.BooleanField(default=False)
