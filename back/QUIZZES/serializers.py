cd0a62a5e86ce50a395c624f93a3419d2a45a694
sonyujeong <sonyujeong18@amsquare.ai>
2025-03-17 18:02:18 +0900

    feat: 퀴즈 목록 조회 시, item을 모두 조회하는 대신 퀴즈 id만 가져오도록 수정

--- back/QUIZZES/serializers.py
+++ back/QUIZZES/serializers.py
@@ -42,3 +42,8 @@ class QuizSerializer(serializers.ModelSerializer):
     items = list(obj.items.all())
     random.shuffle(items)
     return QuizItemSerializer(items, many=True).data
+  
+class QuizListSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Quiz
+    fields = ('id',)
