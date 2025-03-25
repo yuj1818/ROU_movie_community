369130293839abab6845583e873c61c8ae8cd689
sonyujeong <sonyujeong18@amsquare.ai>
2025-03-25 17:48:41 +0900

    feat: google callback api에서 이메일 정보 받아와 path parameter로 추가 정보 기입 페이지에 전달

--- front/src/pages/auth/SocialCallbackPage.jsx
+++ front/src/pages/auth/SocialCallbackPage.jsx
@@ -1,22 +1,29 @@
 import { useEffect } from 'react';
 import { googleLogin } from '../../utils/authApi';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { setEmail } from '../../stores/auth';
+import { setCookie } from '../../utils/cookie';
 
 const SocialCallbackPage = () => {
+  const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
   const onGoogleLogin = async () => {
-    const params = new URLSearchParams(window.location.search);
-    const code = params.get('code');
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
+    const code = searchParams.get('code');
 
     const res = await googleLogin({ code });
     if (res.status == 200) {
-      console.log(res.data);
+      const token = res.data.key;
+      const user = res.data.user;
+      setCookie('token', `Token ${token}`, { path: '/' });
+      setCookie('userId', user, { path: '/' });
+      navigate('/');
     } else if (res.status == 202) {
       dispatch(setEmail(res.data.email));
       // 추가 정보 기입 페이지로 이동
+      navigate({ pathname: '/signup', search: `?isSocial=${true}` });
     }
   };
 
