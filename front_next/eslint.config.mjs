a309d13b4c36375bd60bf06a79d3d58281337473
sonyujeong <sonyujeong18@amsquare.ai>
2026-01-29 16:57:03 +0900

    feat: 영화 트렌드 데이터 캐러셀 컴포넌트 구현

--- front_next/eslint.config.mjs
+++ front_next/eslint.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig, globalIgnores } from 'eslint/config';
 import nextVitals from 'eslint-config-next/core-web-vitals';
 import nextTs from 'eslint-config-next/typescript';
 import prettier from 'eslint-config-prettier';
+import pluginQuery from '@tanstack/eslint-plugin-query';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
   ...prettier,
+  ...pluginQuery,
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
