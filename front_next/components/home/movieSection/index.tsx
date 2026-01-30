2e024469282c4bce050dd09bc9aacf6aeae98853
sonyujeong <sonyujeong18@amsquare.ai>
2026-01-30 10:01:20 +0900

    feat: 장르별, 정렬 기준 별 영화 목록 조회 api route 설정

---
+++ front_next/components/home/movieSection/index.tsx
@@ -0,0 +1,5 @@
+
+
+export default function MovieSections() {
+  return <></>;
+}
--- front_next/lib/movie.ts
+++ front_next/lib/movie.ts
@@ -1,9 +1,27 @@
+import { Tag } from '@/types/movie';
+
 const URL = process.env.NEXT_PUBLIC_BASE_URL + '/api/movies';
 
 export const getTrendMovieList = async () => {
   const res = await fetch(`${URL}/trends`, { method: 'GET' });
 
   if (!res.ok) throw new Error('트렌드 영화 목록 조회 실패');
+
+  return res.json();
+};
+
+export const getSortedMovieList = async (key: Pick<Tag, 'key'>) => {
+  const res = await fetch(`${URL}/sort?key=${key}`, { method: 'GET' });
+
+  if (!res.ok) throw new Error(`${key} 정렬 영화 목록 조회 실패`);
+
+  return res.json();
+};
+
+export const getGenreMovieList = async (genreId: number) => {
+  const res = await fetch(`${URL}/genre/${genreId}`, { method: 'GET' });
+
+  if (!res.ok) throw new Error(`장르_${genreId} 영화 목록 조회 실패`);
 
   return res.json();
 };
