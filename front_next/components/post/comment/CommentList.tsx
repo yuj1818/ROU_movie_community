4400341c2af49cb6b4297137a53ea02187354a81
sonyujeong <sonyujeong18@amsquare.ai>
2026-03-13 14:24:56 +0900

    feat: 댓글 목록 조회 무한 스크롤 기능 구현

--- front_next/components/post/comment/CommentList.tsx
+++ front_next/components/post/comment/CommentList.tsx
@@ -3,13 +3,19 @@
 import { Separator } from '@/components/ui/separator';
 import { getCommentList } from '@/lib/client/post';
 import { Comment } from '@/types/post';
-import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery } from '@tanstack/react-query';
 import { useParams } from 'next/navigation';
 import CommentInfo from './CommentInfo';
+import CommentTextarea from './CommentTextarea';
+import { useEffect, useState, useRef, useCallback } from 'react';
+import { useSession } from 'next-auth/react';
+import { Button } from '@/components/ui/button';
 
 export default function CommentList() {
+  const session = useSession();
   const params = useParams();
   const reviewId = Number(params.reviewId);
+  const observerRef = useRef<HTMLDivElement | null>(null);
   const { data, fetchNextPage, hasNextPage, isFetchingNextPage, status } =
     useInfiniteQuery({
       queryKey: ['post', reviewId, 'comments'],
@@ -23,17 +29,44 @@ export default function CommentList() {
       },
     });
 
+  const [content, setContent] = useState('');
+
   const comments = data?.pages.flatMap((page) => page.results) ?? [];
+
+  const handleObserver = useCallback(
+    (entries: IntersectionObserverEntry[]) => {
+      const target = entries[0];
+      if (target.isIntersecting && hasNextPage && !isFetchingNextPage) {
+        fetchNextPage();
+      }
+    },
+    [fetchNextPage, hasNextPage, isFetchingNextPage],
+  );
+
+  useEffect(() => {
+    const target = observerRef.current;
+    if (!target) return;
+
+    const option = { root: null, rootMargin: '200px', threshold: 0 };
+    const observer = new IntersectionObserver(handleObserver, option);
+    observer.observe(target);
+
+    return () => {
+      observer.unobserve(target);
+    };
+  }, [handleObserver]);
 
   return (
     <div className="w-full flex flex-col gap-2">
-      <span className="font-semibold">{`댓글 (${comments.length})`}</span>
+      <span className="font-semibold">{`댓글 (${data?.pages[0]?.count ?? 0})`}</span>
       <Separator className="bg-white" />
-      <ul className="w-full flex flex-col gap-2">
+      <div className="w-full flex flex-col gap-2">
         {comments.map((comment: Comment) => (
           <CommentInfo key={comment.id} {...comment} />
         ))}
-      </ul>
+        {hasNextPage && <div ref={observerRef} className="h-1" />}
+        {isFetchingNextPage && <div>Loading...</div>}
+      </div>
     </div>
   );
 }
