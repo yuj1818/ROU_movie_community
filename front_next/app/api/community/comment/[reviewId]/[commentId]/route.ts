36a93d5f41a6ab753a7d8c06ca958c7809217c3c
sonyujeong <sonyujeong18@amsquare.ai>
2026-03-16 13:46:54 +0900

    feat: 댓글 삭제 기능 구현

--- front_next/app/api/community/comment/[reviewId]/[commentId]/route.ts
+++ front_next/app/api/community/comment/[reviewId]/[commentId]/route.ts
@@ -29,3 +29,50 @@ export async function PUT(req: NextRequest) {
     return NextResponse.json({ error: 'Server Error' }, { status: 500 });
   }
 }
+
+export async function POST(req: NextRequest) {
+  const session = await getServerSession(authOptions);
+
+  if (!session)
+    return NextResponse.json({ error: 'Unauthenticated' }, { status: 401 });
+
+  const body = await req.json();
+
+  try {
+    const pathname = req.nextUrl.pathname;
+    const res = await fetch(`${URL}${pathname}/`, {
+      method: 'POST',
+      headers: {
+        Authorization: `Token ${session.backendToken}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    });
+
+    const data = await res.json();
+    return NextResponse.json(data, { status: res.status });
+  } catch (err) {
+    return NextResponse.json({ error: 'Server Error' }, { status: 500 });
+  }
+}
+
+export async function DELETE(req: NextRequest) {
+  const session = await getServerSession(authOptions);
+
+  if (!session)
+    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+
+  try {
+    const pathname = req.nextUrl.pathname;
+    const res = await fetch(`${URL}${pathname}/`, {
+      method: 'DELETE',
+      headers: {
+        Authorization: session ? `Token ${session.backendToken}` : '',
+      },
+    });
+
+    return new NextResponse(null, { status: res.status });
+  } catch (err) {
+    return NextResponse.json({ error: 'Server Error' }, { status: 500 });
+  }
+}
